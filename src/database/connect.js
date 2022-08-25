dc6c10ffe5f386d796887bbc89f2b9ef5cea890b
Guilherme <guilherme.martinez067@gmail.com>
2022-08-25 11:01:11 -0400

    primeiro commit

---
+++ src/database/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectToDatabase = async () => {
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.q2kilm4.mongodb.net/?retryWrites=true&w=majority`, 
+    (error) => {
+        if (error) {
+            return console.log(
+                "Ocorreu um erro ao se conectar com o banco de dados: ",
+                error
+            )
+        }
+
+        return console.log("Conexão ao banco de dados realizada com sucesso!");
+    });
+};
+
+module.exports = connectToDatabase;
